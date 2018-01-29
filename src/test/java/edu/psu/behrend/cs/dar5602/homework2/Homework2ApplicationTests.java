d2bc08790c9fc3d7ae700924acdc052224a52e73
Dustin Reimold <dustinreimold@gmail.com>
2018-01-29 12:45:51 -0500

    added dependencies and initial files

---
+++ src/test/java/edu/psu/behrend/cs/dar5602/homework2/Homework2ApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.psu.behrend.cs.dar5602.homework2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Homework2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
