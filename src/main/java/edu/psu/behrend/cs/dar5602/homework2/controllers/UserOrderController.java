883a20bf8ca7319fa95f47d12d47fb522e5d8cd7
Dustin Reimold <dustinreimold@gmail.com>
2018-01-29 13:40:52 -0500

    created update order and create order methods

--- src/main/java/edu/psu/behrend/cs/dar5602/homework2/controllers/UserOrderController.java
+++ src/main/java/edu/psu/behrend/cs/dar5602/homework2/controllers/UserOrderController.java
@@ -1,15 +1,64 @@
 package edu.psu.behrend.cs.dar5602.homework2.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.psu.behrend.cs.dar5602.homework2.dtos.UserOrder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.HashMap;
 
 @RestController
 public class UserOrderController {
 
-    @GetMapping (path = "/createOrder")
-    public int createOrder(){
+    private HashMap<Integer, UserOrder> orderHashMap;
 
 
-        return 0;
+    /**
+     * Method to get items available to order
+     * @return inventory string
+     */
+    @GetMapping (path = "/inventory")
+    public String getInventory(){
+        String inventory = "Small Tables; $350" +
+                "Large Tables; $750" +
+                "Long Tables; $500" +
+                "Round Tables; $1000";
+
+        return inventory;
     }
+
+
+    /**
+     * Creates a new order for a new customer
+     * @param order order method
+     * @return maxKey
+     */
+    @PostMapping(path = "/createOrder")
+    public int createOrder(@RequestParam UserOrder order){
+        if(this.orderHashMap.isEmpty()){
+            this.orderHashMap = new HashMap<>();
+        }
+
+        int maxKey = 0;
+
+        if(!this.orderHashMap.isEmpty()){
+            maxKey = Collections.max( this.orderHashMap.keySet() ) + 1;
+        }
+
+        this.orderHashMap.put(maxKey, order);
+
+        return maxKey;
+    }
+
+    /**
+     * Method to update the users order to comply with what they want
+     * @param id is the id associated with the user
+     * @param order is the new order that they want
+     */
+    @PutMapping (path = "/updateOrder")
+    public void updateOrder(@RequestParam (value = "id") int id, @RequestParam UserOrder order){
+        if(this.orderHashMap.containsKey(id)){
+            this.orderHashMap.put(id,order);
+        }
+    }
+    @
 }
